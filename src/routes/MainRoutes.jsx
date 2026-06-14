44d21855995329590193191a8799146e3a44c7f3
Rohit kumar <rohit5592006@gmail.com>
2026-06-14 20:10:25 +0530

    added protected routes

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -10,21 +10,30 @@ import Checkout from '../pages/Checkout'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
 import MyAccount from '../pages/MyAccount'
+import ProtectedRoutes from "./../components/ProtectedRoutes"
 
 export const MainRoutes = createBrowserRouter([
     // {path:"*",element:},
-    {path:"/",element:<RootLayout/>,
-        
-        children:[
-            {path:"/",element:<Home/>},
-            {path:"/shop",element:<Shop/>},
-            {path:"/product-details/:id",element:<ProductDetails/>},
-            {path:"/cart",element:<Cart/>},
-            {path:"/wishlist",element:<Wishlist/>},
-            {path:"/checkout",element:<Checkout/>},
-            {path:"/login",element:<Login/>},
-            {path:"/signup",element:<Signup/>},
-            {path:"/my-account",element:<MyAccount/>},
+    {
+        path: "/", element: <RootLayout />,
+
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/shop", element: <Shop /> },
+            { path: "/shop/:category", element: <Shop /> },
+            { path: "/product-details/:id", element: <ProductDetails /> },
+            { path: "/cart", element: <Cart /> },
+            { path: "/wishlist", element: <Wishlist /> },
+            { path: "/checkout", element: <Checkout /> },
+            { path: "/login", element: <Login /> },
+            { path: "/signup", element: <Signup /> },
+            {
+                element: <ProtectedRoutes />,
+                children: [
+                    { path: "/my-account", element: <MyAccount /> }
+                ]
+            },
+
 
         ]
     }
