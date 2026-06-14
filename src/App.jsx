e5df12b740d34195481552631f87b9f381fe254d
Rohit kumar <rohit5592006@gmail.com>
2026-06-14 20:11:40 +0530

    added toastcontainer and store provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import { RouterProvider } from "react-router-dom"
 import './assets/sass/style.scss'
 import './assets/css/style.css'
 import { MainRoutes } from "./routes/MainRoutes"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux"
+import store from "./store/store"
 function App() {
-  return (
-    <RouterProvider router={MainRoutes} />
+  return (<>
+
+    <Provider store={store}>
+      <RouterProvider router={MainRoutes} /></Provider>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+    />
+  </>
   )
 }
 
